5699905daa316d66ce3674d7b901a5e706598fbe
Carl Karama <carl.karama@findex.com.au>
2023-11-08 16:55:10 +1100

    ref: refactor by extraction

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import Header from "./Header"
 import Spinner from "./Spinner";
 
 function App() {
+  
+  const sprintNames: string[] =[
+    "JST Collider", "Love/Hate Jira", "Bento Box Sprints", "Collins Street Retros", 
+    "Billable Hours", "But it's not a small PR", "No Code Violations", "Close Sprint Last Strike",
+    "Velocity go live", "Sudan's Last PR", "Is it espresso or expresso", "Magical Thinking",
+    "Softer Deadlines", "Approved PR", "Bulky PR", "MFE Matrix", "Vite is the way", "Ng life"
+  ];
+
   return (
     <div>
       <Header></Header>
-      <Spinner></Spinner>
+      <Spinner sprintNames={sprintNames}></Spinner>
     </div>
     
   );
 }
 
-export default App
+export default App  
